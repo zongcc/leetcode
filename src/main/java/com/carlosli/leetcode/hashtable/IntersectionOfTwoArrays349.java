c476cbdf29a9b2c502eaf3bc2b9ee46359a7a698
yulongli <yulongli@sohu-inc.com>
2017-01-10 15:11:11 +0800

    Intersection Of Two Arrays 349

--- src/main/java/com/carlosli/leetcode/hashtable/IntersectionOfTwoArrays349.java
+++ src/main/java/com/carlosli/leetcode/hashtable/IntersectionOfTwoArrays349.java
@@ -61,6 +61,38 @@ public class IntersectionOfTwoArrays349 {
         return result;
     }
 
+    public static int[] intersection3(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public static boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         int[] nums1 = new int[]{1, 2, 2, 3, 4, 5};
